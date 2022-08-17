6a1c56afc79d348fcba1e9f0d92def5867a8b569
SuJin <1175736406@qq.com>
2022-08-17 22:53:24 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author sjStart
+ * @create 2022-08-17 22:46
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("你好git");
+    }
+}
